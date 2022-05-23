babe938d8d86c7f70a55cdc99058c5da15157561
Alex Fernandes <alex.soares1414@gmail.com>
2022-05-23 03:07:12 -0300

    primeiro consumo de api, testes

--- src/views/Contact/index.tsx
+++ src/views/Contact/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, FormEvent } from "react";
 import { Container } from "./style";
 import { Link } from "react-router-dom";
-
+import { api } from "../../services/api";
 
 interface IData {
   name: string;
@@ -10,31 +10,53 @@ interface IData {
 
 const Contact: React.FC = () => {
   const [data, setData] = useState<IData>({} as IData);
+  const [submit, setSubmit] = useState(false);
+
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      api
+        .post("5a65e7d2-dafe-49d2-af09-34d42df11c39", data)
+        .then((response) => {
+          if (response.status === 200) {
+            setSubmit(true);
+          }
+        }).catch(err => console.log(err))
+    },
+    [data]
+  );
+
   return (
     <Container>
       <div className="form-wrapper">
         <h1>Contact</h1>
         <div className="card">
-          <form onSubmit={() => {}}>
-            <fieldset>
-              <label htmlFor="">Nome</label>
-              <input
-                type="text"
-                placeholder="Insira seu nome"
-                onChange={(e) => setData({ ...data, name: e.target.value })}
-              />
-              <br />
-              <label htmlFor="">Email</label>
-              <input
-                type="email"
-                placeholder="Insira seu e-mail"
-                onChange={(e) => setData({ ...data, email: e.target.value })}
-              />
-              <br />
-              <input type="submit" value="Cadastrar" />
-              <br />
-            </fieldset>
-          </form>
+          {submit ? (
+            <div>
+              <h2>Enviado</h2>
+            </div>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <label htmlFor="">Nome</label>
+                <input
+                  type="text"
+                  placeholder="Insira seu nome"
+                  onChange={(e) => setData({ ...data, name: e.target.value })}
+                />
+                <br />
+                <label htmlFor="">Email</label>
+                <input
+                  type="email"
+                  placeholder="Insira seu e-mail"
+                  onChange={(e) => setData({ ...data, email: e.target.value })}
+                />
+                <br />
+                <input type="submit" value="Cadastrar" />
+                <br />
+              </fieldset>
+            </form>
+          )}
         </div>
         <Link to="/">Acessar pagina Home</Link>
       </div>
