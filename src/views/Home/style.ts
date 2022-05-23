d8334a83d32026f689a58ae5e5d611f047457d98
Alex Fernandes <alex.soares1414@gmail.com>
2022-05-23 00:00:32 -0300

    Montando estrutura inicial

---
+++ src/views/Home/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components"; //? styled-react
+
+export const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  border: solid red 1px;
+  box-sizing: border-box ;
+`;
